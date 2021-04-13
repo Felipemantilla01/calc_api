a5576319194526e893d2d7eb12910f00f4e087d9
felipe <felipemantillagomez@gmail.com>
2021-04-13 14:54:54 -0700

    added user login

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
 /**
  * Copyright by Felipe.
  */
-
+require('rootpath')();
 const express = require('express');
 const app = express();
 
 var bodyParser = require('body-parser');
- 
+const basicAuth = require('_helpers/basic-auth');
+const errorHandler = require('_helpers/error-handler');
+
 const db = require('./app/config/db.config.js');
   
 // force: true will drop the table if it already exists
-// db.sequelize.sync({force: true}).then(() => {
-//   console.log('Drop and Resync with { force: true }');
-// }); 
+db.sequelize.sync({force: false}).then(() => {
+  console.log('Drop and Resync with { force: true }');
+}); 
 
 let router = require('./app/routers/router.js');
 
 const cors = require('cors')
 const corsOptions = {
-  origin: 'http://34.229.63.119',
+  origin: 'http://localhost:4200',
   optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(basicAuth);
+
 app.use('/', router);
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
 
 // Create a Server
 const server = app.listen(8080, function () {
